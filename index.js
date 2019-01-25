7068775b787e9d00d31b4e5bb5b4db2cd61fecc7
Sherin Binu <sherinbinu@hotmail.com>
2019-01-25 22:36:09 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+require('opn')('https://egoist.sh', {
+  wait: false
+});
+
+const words = [
+  'osu!',
+  'gahaha!',
+  'moshimoshi?',
+  'hentai!',
+  'dame!',
+  'yada!',
+  'kimochi..',
+  'baka!',
+  'nande?',
+  'yamedei!',
+  'arigato!',
+  'urusai!'
+];
+
+console.log(words[Math.floor(Math.random() * words.length)]);
